94d850525efda87cbb2bea1dd08cb34d9304c132
ridvankoseler <ridvankoseler@gmail.com>
2022-08-09 01:13:09 +0300

    first commit

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,20 @@
+import { languages } from "../../helpers/data";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className='cards-area-container'>
+      <div className='bars'></div>
+      <h1 className='language-title'>LANGUAGES</h1>
+      <div className='cards-container'>
+        {languages.map((item,index) => {
+          return ( <Item card={item}/>
+            
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
