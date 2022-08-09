185c14357fcfdbf03b15a7b3bbb90db84126f46f
ridvankoseler <ridvankoseler@gmail.com>
2022-08-09 23:39:36 +0300

    Updated.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import './App.css';
 import Card from './components/card/Card';
 import Header from './components/header/Header';
-import Item from './components/item/Item';
+
 
 function App() {
   return (
